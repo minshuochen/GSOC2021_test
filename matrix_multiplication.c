d1a7393f5f70c9f3b3953f50129b95fcf90276e7
Minshuo <minshuochen@gmail.com>
2021-04-11 21:43:37 -0400

    Update matrix_multiplication.c

--- matrix_multiplication.c
+++ matrix_multiplication.c
@@ -49,9 +49,9 @@ void display(int result[][10], int row, int column) {
 
 int main() {
    int first[10][10], second[10][10], result[10][10], r1, c1, r2, c2;
-   printf("Enter rows and column for the first matrix: ");
+   printf("Enter rows and columns for the first matrix: ");
    scanf("%d %d", &r1, &c1);
-   printf("Enter rows and column for the second matrix: ");
+   printf("Enter rows and columns for the second matrix: ");
    scanf("%d %d", &r2, &c2);
 
    // Taking input until
